6f4aadc62c473ab53b6347527ba549684ea578ac
Andrew Fan <andrewsuicune@gmail.com>
2018-05-21 17:23:45 -0400

    CVEParser halfway done

--- src/main/java/com/spnthreatviz/CVEParser.java
+++ src/main/java/com/spnthreatviz/CVEParser.java
@@ -5,6 +5,7 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonArray;
+import com.mysql.cj.xdevapi.JsonString;
 
 /*
  * This class is based on https://stackoverflow.com/questions/11428329/
@@ -44,8 +45,13 @@ public class CVEParser {
     public void getCVEItems(JsonArray cveItems) throws STVException.CVEParsingException  {
         for (JsonElement cveElement : cveItems) {
             JsonObject cve = cveElement.getAsJsonObject(); //convert array element to JsonObject
-            CVEObject cveObj = parseCVE(cve); //parse to a POJO
-            //Handle Database entry
+            try {
+                CVEObject cveObj = parseCVE(cve); //parse to a POJO
+                //Handle Database entry
+            } catch (STVException.CVEParsingException e) {
+                //Note that there was an error when parsing, but continue onwards with the rest of the entries.
+                System.out.println(e.getMessage());
+            }
         }
     }
 
@@ -59,7 +65,77 @@ public class CVEParser {
         CVEObject toReturn = new CVEObject();
 
         //One by one, obtain the important fields.
+        //To avoid exceptions, unwrap one layer at a time.
 
+        //Unwrap main CVE component
+        JsonObject cve_main = cveItem.get("cve").getAsJsonObject();
+        if (cve_main == null) { throw new STVException.CVEParsingException("parseCVE: input missing cve field.", new Throwable()); }
+
+        //Vendor, product name, and version data
+        JsonObject layer_affects = cve_main.get("affects").getAsJsonObject();
+        if (layer_affects == null) { throw new STVException.CVEParsingException("parseCVE: input missing affects field.", new Throwable()); }
+        JsonObject layer_vendor = layer_affects.get("vendor").getAsJsonObject();
+        if (layer_vendor == null) { throw new STVException.CVEParsingException("parseCVE: input missing vendor field.", new Throwable()); }
+        JsonArray layer_vendordata = layer_vendor.get("vendor_data").getAsJsonArray();
+
+        //If vendor_data is empty for whatever reason, leave the fields as empty strings.
+        //Do not throw an exception if it is empty, as there may be a use for it being empty.
+        if (layer_vendordata.size() > 0) {
+            //We will only ever choose the first vendor in the array for simplicity.
+            JsonObject layer_vendorobj = layer_vendordata.get(0).getAsJsonObject();
+            String vendor_name = layer_vendorobj.get("vendor_name").getAsString();
+            toReturn.setVendor(vendor_name);
+
+            JsonObject layer_product = layer_vendorobj.get("product").getAsJsonObject();
+            //Only set product name and version values if the product field exists.
+            //However, do not throw an exception if it is null, as that's still technically 'valid'
+            if (layer_product != null) {
+                JsonArray layer_productdata = layer_product.get("product_data").getAsJsonArray();
+
+                //Like with vendor_data, we will only choose the first product in the array for simplicity, and will leave the fields as empty strings if it is empty.
+                if (layer_productdata.size() > 0) {
+                    JsonObject layer_productobj = layer_productdata.get(0).getAsJsonObject();
+                    String product_name = layer_productobj.get("product_name").getAsString();
+                    toReturn.setProductName(product_name);
+
+                    //Handle version object.
+                    JsonObject layer_version = layer_productobj.get("version").getAsJsonObject();
+                    if (layer_version != null) {
+                        JsonArray version_data = layer_version.get("version_data").getAsJsonArray();
+                        String[] version_values = new String[version_data.size()];
+                        //Iterate over version_data and insert values into array.
+                        for (int i = 0; i < version_data.size(); i += 1) {
+                            JsonObject version_value = version_data.get(i).getAsJsonObject();
+                            version_values[i] = version_value.get("version_value").getAsString();
+                        }
+                        toReturn.setVersionValues(version_values);
+                    }
+                }
+            }
+        }
+
+        //Next we handle the description.
+        JsonObject layer_description = cve_main.get("description").getAsJsonObject();
+        if (layer_description != null) {
+            //If it does not exist, just leave description blank.
+            JsonArray layer_descriptiondata = layer_description.get("description_data").getAsJsonArray();
+            //We will only use the first element.
+            if (layer_descriptiondata.size() > 0) {
+                JsonObject descriptionobj = layer_descriptiondata.get(0).getAsJsonObject();
+                String description = descriptionobj.get("value").getAsString();
+                toReturn.setDescription(description);
+            }
+        }
+
+        //Next we handle everything related to the impact of the vulnerability
+        JsonObject cve_impact = cveItem.get("impact").getAsJsonObject();
+        if (cve_impact == null) { throw new STVException.CVEParsingException("parseCVE: input missing impact field.", new Throwable()); }
+
+
+        //Finally we handle the dates
+        String pubdate_raw = cveItem.get("publishedDate").getAsString();
+        String moddate_raw = cveItem.get("lastModifiedDate").getAsString();
+        //Substrings utilized for storage (YYYY-MM-DD)
 
         return toReturn;
     }
